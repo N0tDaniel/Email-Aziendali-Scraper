49e1ee71f5c8d7cca4e866a83f70f9d6ad977171
NotDaniel <42096350+N0tDaniel@users.noreply.github.com>
2023-01-27 00:17:32 +0100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,33 +1,42 @@
 import re
 import requests
+import csv
 from bs4 import BeautifulSoup
 
-
-sites = input("Inserisci gli URL dei siti web separati da virgola:").split(',')
-file_name = input("Inserisci il nome del file in cui salvare gli indirizzi email:")
+file_name = input("Inserisci il nome del file che contiene gli URL dei siti web:")
+output_file = input("Inserisci il nome del file CSV in cui salvare gli indirizzi email:")
 
 emails = []
 
 
+with open(file_name, 'r') as f:
+    sites = f.read().splitlines()
+
+
 for site in sites:
-    response = requests.get(site)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        response = requests.get(site)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    for link in soup.find_all('a'):
-        email = link.get('href')
-        if email and 'mailto:' in email:
-            email = email.replace('mailto:','')
-            emails.append(email)
+        for link in soup.find_all('a'):
+            email = link.get('href')
+            if email and 'mailto:' in email:
+                email = email.replace('mailto:','')
+                emails.append(email)
 
-    text = soup.get_text()
-    emails.extend(re.findall(r'[\w\.-]+@[\w\.-]+', text))
+        text = soup.get_text()
+        emails.extend(re.findall(r'[\w\.-]+@[\w\.-]+', text))
+    except:
+        print(f"Impossibile raggiungere il sito web: {site}")
 
 # Rimuovi eventuali duplicati
 emails = list(set(emails))
 
-# Apri un file per scrivere gli indirizzi email
-with open(file_name, 'w') as f:
+# Apre un file CSV per scrivere gli indirizzi email
+with open(output_file, 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Email'])
     for email in emails:
-        f.write(email + '\n')
+        writer.writerow([email])
 
 print(f'Trovati {len(emails)} indirizzi email')
